c98af67041d50b86acd13637dd1fbe92b3536350
josebon2005 <jbonillas1@miumg.edu.gt>
2026-03-03 20:42:24 -0600

    commit 2

--- universidad/Models/Alumno/views.py
+++ universidad/Models/Alumno/views.py
@@ -1,54 +1,44 @@
-from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib import messages
 from .models import Alumno
 from .forms import AlumnoForm
 
+
 def alumno_list(request):
-    query   = request.GET.get('q', '')
     alumnos = Alumno.objects.all()
+    return render(request, 'alumno_list.html', {'alumnos': alumnos})
 
-    if query:
-        alumnos = alumnos.filter(first_name__icontains=query) \
-                | alumnos.filter(last_name__icontains=query)  \
-                | alumnos.filter(email__icontains=query)
-
-    return render(request, 'alumno/list.html', {
-        'alumnos': alumnos,
-        'query':   query
-    })
 
 def alumno_detail(request, pk):
     alumno = get_object_or_404(Alumno, pk=pk)
-    return render(request, 'alumno/detail.html', {'alumno': alumno})
+    return render(request, 'alumno_detail.html', {'alumno': alumno})
+
 
 def alumno_create(request):
-    form = AlumnoForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        messages.success(request, 'Alumno registrado correctamente.')
-        return redirect('alumno:list')
-    return render(request, 'alumno/form.html', {
-        'form':  form,
-        'title': 'Nuevo Alumno'
-    })
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('alumno:list')
+    else:
+        form = AlumnoForm()
+    return render(request, 'alumno_form.html', {'form': form})
+
 
 def alumno_edit(request, pk):
     alumno = get_object_or_404(Alumno, pk=pk)
-    form   = AlumnoForm(request.POST or None, instance=alumno)
-    if form.is_valid():
-        form.save()
-        messages.success(request, 'Alumno actualizado correctamente.')
-        return redirect('alumno:list')
-    return render(request, 'alumno/form.html', {
-        'form':  form,
-        'title': f'Editar: {alumno.first_name} {alumno.last_name}'
-    })
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST, instance=alumno)
+        if form.is_valid():
+            form.save()
+            return redirect('alumno:list')
+    else:
+        form = AlumnoForm(instance=alumno)
+    return render(request, 'alumno_form.html', {'form': form})
+
 
 def alumno_delete(request, pk):
     alumno = get_object_or_404(Alumno, pk=pk)
     if request.method == 'POST':
         alumno.delete()
-        messages.success(request, 'Alumno eliminado.')
         return redirect('alumno:list')
-    return render(request, 'alumno/confirm_delete.html', {'alumno': alumno})
+    return render(request, 'alumno_confirm_delete.html', {'alumno': alumno})
