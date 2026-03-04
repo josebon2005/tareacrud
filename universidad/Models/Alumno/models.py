7dcf02d172a16557160c3b728aaf4f9fc15a523f
josebon2005 <jbonillas1@miumg.edu.gt>
2026-03-03 18:32:45 -0600

    commit 1

--- universidad/Models/Alumno/models.py
+++ universidad/Models/Alumno/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Alumno(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -15,3 +16,47 @@ class Alumno(models.Model):
 
     class Meta:
         db_table = 'alumno'
+
+
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    creditos = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Catedratico(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class AsignacionCurso(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    catedratico = models.ForeignKey(Catedratico, on_delete=models.CASCADE)
+    horario = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.curso} - {self.catedratico}"
+
+
+class InscripcionAlumno(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    fecha = models.DateField()
+
+    def __str__(self):
+        return f"{self.alumno} - {self.curso}"
+
+
+class Notas(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.alumno} - {self.curso} - {self.nota}"
--- universidad/settings.py
+++ universidad/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'universidad',
         'USER': 'root',
-        'PASSWORD': 'root1234',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
